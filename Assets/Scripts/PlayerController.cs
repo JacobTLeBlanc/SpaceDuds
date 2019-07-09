98a950e5404bd02222d405422affc676b32c5530
JacobTLeBlanc <leblancthomasjacob@gmail.com>
2019-07-08 21:34:16 -0300

    Added spaceship + basic movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5.0f;
+
+    Rigidbody2D rb2d;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        Vector2 move = new Vector2(horizontal, vertical);
+
+        Vector2 position = rb2d.position;
+        position = position + move * speed * Time.deltaTime;
+        rb2d.position = position;
+        
+    }
+}
