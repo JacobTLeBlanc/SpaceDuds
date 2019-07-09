9a7c9ddcd21f3d426a679e960b534ace442633e1
JacobTLeBlanc <leblancthomasjacob@gmail.com>
2019-07-09 20:29:04 -0300

    Added Scrolling and Infinite background

---
+++ Assets/Scripts/Repeating.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Repeating : MonoBehaviour
+{
+    BoxCollider2D boxCollider;
+    float verticalLength;
+    float horizontalOffset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        boxCollider = GetComponent<BoxCollider2D>();
+        verticalLength = boxCollider.size.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y < -verticalLength)
+        {
+            RepositionBackground();
+        }
+    }
+
+    void RepositionBackground()
+    {
+        horizontalOffset = Random.Range(-10.0f, 10.0f);
+
+        Vector2 groundOffset = new Vector2(horizontalOffset, verticalLength * 2f);
+        transform.position = (Vector2) transform.position + groundOffset;
+    }
+}
---
+++ Assets/Scripts/ScrollingObject.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollingObject : MonoBehaviour
+{
+    Rigidbody2D rb2d;
+
+    public float scrollingSpeed = -3.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+
+        rb2d.velocity = new Vector2(0, scrollingSpeed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
