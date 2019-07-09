8a7283926ce7f6081a4b21ebb465fd0d5d8a83c1
JacobTLeBlanc <leblancthomasjacob@gmail.com>
2019-07-09 13:52:58 -0300

    Added bullets

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,6 +6,7 @@ public class PlayerController : MonoBehaviour
 {
     public float speed = 5.0f;
 
+    public GameObject bulletPrefab;
     Rigidbody2D rb2d;
 
     // Start is called before the first frame update
@@ -24,6 +25,20 @@ public class PlayerController : MonoBehaviour
         Vector2 position = rb2d.position;
         position = position + move * speed * Time.deltaTime;
         rb2d.position = position;
-        
+
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            Fire();
+        }
     }
+
+    void Fire()
+    {
+        GameObject bulletObject = Instantiate(bulletPrefab, rb2d.position + Vector2.up * 0.5f, Quaternion.identity);
+        Vector2 direction = new Vector2(0, 1);
+
+        Projectile projectile = bulletObject.GetComponent<Projectile>();
+        projectile.Launch(direction, 300.0f);
+    }
+
 }
---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    Rigidbody2D rb2d;
+    
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.magnitude > 1000.0f)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void Launch(Vector2 direction, float force)
+    {
+        rb2d.AddForce(direction * force);
+    }
+}
