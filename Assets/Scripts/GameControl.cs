f563b7e9348168afe5517aa8fd38cd7bc85955d9
JacobTLeBlanc <leblancthomasjacob@gmail.com>
2019-07-12 14:11:14 -0300

    Added GAme Control Script

---
+++ Assets/Scripts/GameControl.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameControl : MonoBehaviour
+{
+    public static GameControl instance;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        // Make sure there is only one instance of game control
+        if (instance == null) {
+            instance = this;
+        } else if (instance != this) {
+            Destroy(gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
