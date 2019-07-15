995a8656dfd7174688f101aa9cfb56ab58f02340
JacobTLeBlanc <leblancthomasjacob@gmail.com>
2019-07-15 00:21:34 -0300

    Added Comments to Asteroid Script

--- Assets/Scripts/Asteroid.cs
+++ Assets/Scripts/Asteroid.cs
@@ -6,41 +6,49 @@ public class Asteroid : MonoBehaviour
 {
     Rigidbody2D rb2d;
 
+    // Timer Length
     public float timerLength = 5.0f;
-    float destroyTimer = 0.0f;
+    float destroyTimer = 0.0f; // Timer
     
 
     // Start is called before the first frame update
     void Awake()
     {
-        rb2d = GetComponent<Rigidbody2D>();
-        destroyTimer = timerLength;
+        rb2d = GetComponent<Rigidbody2D>(); // Get RigidBody2D component
+        destroyTimer = timerLength; // Set timer to full length
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Update timer every frame
         destroyTimer -= Time.deltaTime;
+        // If timer reaches 0, destroy asteroid
         if (destroyTimer < 0)
         {
             Destroy(gameObject);
         }
     }
 
-    // Destroy on collison
+    // Detect collision
     void OnCollisionEnter2D(Collision2D other)
     {
+        // Check if object is bullet or already destroyed asteroid
         Projectile bullet = other.gameObject.GetComponent<Projectile>();
         DestroyedAsteroid destroyed = gameObject.GetComponent<DestroyedAsteroid>();
 
         if (bullet != null && destroyed == null)
         {
-            int Rand = Random.Range(1, 3);
+            // Turn astroid into 1-3 pieces
+            int Rand = Random.Range(1, 4);
 
+            // Create new asteroids
             for (int i = 0; i < Rand; i++)
             {
                 breakAsteroid();
             }
+
+            // Destroy current asteroid
             Destroy(gameObject);
         }
     }
@@ -49,7 +57,11 @@ public class Asteroid : MonoBehaviour
     {
         // Create new asteroid
         GameObject newAst = Instantiate(gameObject, this.transform.position, Quaternion.identity);
+
+        // Scale down new asteroids
         newAst.transform.localScale = this.transform.localScale * Random.Range(0.3f, 0.8f);
+
+        // Add destroyedAsteroid script
         newAst.AddComponent<DestroyedAsteroid>();
     }
 
