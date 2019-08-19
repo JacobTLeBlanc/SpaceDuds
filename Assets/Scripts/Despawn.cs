06a1dfad62fad42ceeef3b4ec8cd846af81b3a08
JacobTLeBlanc <leblancthomasjacob@gmail.com>
2019-08-19 18:05:04 -0300

    Added Comments to all scripts

--- Assets/Scripts/Despawn.cs
+++ Assets/Scripts/Despawn.cs
@@ -4,18 +4,22 @@ using UnityEngine;
 
 public class Despawn : MonoBehaviour
 {
+    // Despawn timer
     float destroyTimer;
     float destroyLength = 10.0f;
 
     void Start()
     {
+        // Start timer
         destroyTimer = destroyLength;
     }
 
     void Update()
     {
+        // Update timer
         destroyTimer -= Time.deltaTime;
 
+        // Despawn when timer reaches 0
         if (destroyTimer < 0)
         {
             Destroy(gameObject);
--- Assets/Scripts/UIBullet.cs
+++ Assets/Scripts/UIBullet.cs
@@ -5,23 +5,30 @@ using UnityEngine.UI;
 
 public class UIBullet : MonoBehaviour
 {
+    // Instance of class
     public static UIBullet instance { get; private set; }
 
+    // Mask
     public Image mask;
+
+    // Original size of mask
     float originalSize;
     
     void Awake()
     {
+        // Make this object the only instance
         instance = this;
     }
 
     void Start()
     {
+        // Get original size
         originalSize = mask.rectTransform.rect.width;
     }
 
     public void SetValue(float value)
     {
+        // Update size according to value
         mask.rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, originalSize * value);
     }
 }
