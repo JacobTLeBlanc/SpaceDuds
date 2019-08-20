09d97c3be47e65eda903a9d3f03de3a2ee68387f
JacobTLeBlanc <leblancthomasjacob@gmail.com>
2019-08-20 20:04:28 -0300

    BossAdded Build 3.0

---
+++ Temp/gradleOut/build/generated/source/buildConfig/release/com/JakenatorStudio/SpaceDuds/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.JakenatorStudio.SpaceDuds;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.JakenatorStudio.SpaceDuds";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "0.1";
+}
