7d7ea450a31bb4e799a0aa702d030f6333c0c728
JacobTLeBlanc <leblancthomasjacob@gmail.com>
2019-09-02 13:35:10 -0700

    Changed All Fonts + Added High on Main menu

---
+++ Assets/Scripts/Game/MenuUI.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuUI : MonoBehaviour
+{
+    public Text highscore;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SaveLoad.Load();
+
+        highscore.text = "High: " + SaveLoad.data.highscore.ToString() + " km";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
